af6960707a7d71149758809500da9b34ba074973
ywataraisb1350-dot <y.watarai.sb1350@gmail.com>
2025-12-16 17:04:37 +0900

    first_comit

---
+++ design_prop.py
@@ -0,0 +1,12 @@
+import math
+import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+from datetime import datetime
+
+#design parameter input zone
+W_ec = 320  #mm
+L_ec = 320  #mm
+
