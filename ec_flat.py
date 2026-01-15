35411d20a31560218cbad59115459bcbe1f8b3db
Ywata <y.watarai.sb1350@gmail.com>
2026-01-15 15:17:11 +0900

    Plossにwick,capつぃか

--- ec_flat.py
+++ ec_flat.py
@@ -74,4 +74,8 @@ def ec_flat(Tec, Tev):
         ec_result.append(current_data)
     df_ec = pd.DataFrame(ec_result)
 
-    return P, T, df_ec, M_dot, Q_ev, Q_gr, P_loss_gr
+    P_cap = 2*p.sigma(Tev)* math.cos(d.contact_angle)/d.r_max_pore
+    P_loss_wick_flat = p.mu_l(Tev)* (d.H_wick- d.H_gr) * M_dot/(d.K_wick* d.A_wick* p.rho_l(Tev)) #
+    P_loss_wick_gr = p.mu_l(Tev)* d.H_gr* M_dot/(d.K_wick* (d.A_wick- d.w_gr* d.L_gr* d.n_gr) * p.rho_l(Tev))
+
+    return P, T, df_ec, M_dot, Q_ev, Q_gr, P_loss_gr, P_loss_wick_flat, P_loss_wick_gr, P_cap
